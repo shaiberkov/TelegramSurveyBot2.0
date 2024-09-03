5241c1200ee3463f5abdcd75446a587792e2737d
shai berkovich <shaiberkov@gmail.com>
2024-09-03 16:43:45 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 
 public class  Main {
+
+    String l="k";
     public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotsApi=new TelegramBotsApi(DefaultBotSession.class);
