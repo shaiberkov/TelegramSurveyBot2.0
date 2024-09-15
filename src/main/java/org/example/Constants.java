c7e65157e00cd594056a2f1967c783ae7d4009d1
shai berkovich <shaiberkov@gmail.com>
2024-09-15 13:58:10 +0300

    .

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final int MIN_SURVEYS = 1;
     public static final int MAX_OPTIONS = 4;
     public static final int MIN_OPTIONS = 2;
-    public static final int MIN_OBSERVER_COUNT_FOR_SURVEY_CREATION =1;
+    public static final int MIN_OBSERVER_COUNT_FOR_SURVEY_CREATION =3;
     //SurveyStatisticsManager Constants
-    public static final int MINUTES_TO_WAIT_TO_SEND_STATS=1;
+    public static final int MINUTES_TO_WAIT_TO_SEND_STATS=5;
 }
